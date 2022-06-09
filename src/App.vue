e98fbda66b24f67b1839964740c8f4e926fc2890
SXZM666 <63348827+SXZM666@users.noreply.github.com>
2022-06-09 10:35:17 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <RouterView />
+</template>
+<script setup lang="ts">
+import { logLabeled } from "./utils/logger";
+logLabeled("I'm ready!  ⸜(๑'ᵕ'๑)⸝⋆*", "log", "", "color: #66ccff");
+</script>
